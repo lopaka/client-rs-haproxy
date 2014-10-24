7f48979da64494591bef79ca0f9139b5e4301571
Lopaka Delp <lopaka@rightscale.com>
2014-10-23 23:05:10 -0700

    Created default wrapper recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,38 @@
 # Cookbook Name:: client-rs-haproxy
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
+chef_gem 'chef-rewind'
+require 'chef/rewind'
+
+updated_haproxy_config = Mash.new(
+  'global' => {
+    'maxconn' => node['haproxy']['global_max_connections'],
+    'user' => node['haproxy']['user'],
+    'group' => node['haproxy']['group'],
+    'log' => "/dev/log syslog info",
+    'daemon' => true,
+    'quiet' => true,
+    'pidfile' => node['haproxy']['pid_file']
+  },
+  'defaults' => {
+    'log' => 'global',
+    'mode' => 'http',
+    'option' => ['httplog', 'dontlognull', 'redispatch'],
+    'balance' => node['haproxy']['balance_algorithm'],
+  },
+  'frontend' => {
+    'all_requests' => {
+      # HTTP bind address
+      "bind #{node['haproxy']['incoming_address']}:#{node['haproxy']['incoming_port']}" => "",
+      'rspidel' => ['^Set-cookie:\ IP=', '^Server:']
+    }
+  }
+)
+
+include_recipe 'rs-haproxy::default'
+
+rewind 'haproxy["set up haproxy.cnf"]' do
+  config updated_haproxy_config
+  action :create
+end
